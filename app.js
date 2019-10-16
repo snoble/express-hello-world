a083c52cd74bd5f7491aca0595478b4022b74948
Steven Klaiber-Noble <steven.noble@gmail.com>
2019-10-16 14:13:52 -0700

    maybe this works

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const app = express();
+const connectionString = require('./secrets.js').connectionString;
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send("Hello from Render!"));
+const pgp = require('pg-promise')(/* options */)
+const db = pgp(connectionString)
+
+
+app.get("/", (req, res) => {
+    db.none('CREATE SCHEMA IF NOT EXISTS cnt').then(d1 => {
+        db.none('CREATE TABLE IF NOT EXISTS cnt (cnt integer, key integer UNIQUE)').then(d2 => {
+            db.one("INSERT INTO cnt.cnt (cnt, key) VALUES (1, 0) \
+            ON CONFLICT (key) DO UPDATE SET cnt=cnt.cnt+1 WHERE cnt.key=0 \
+            RETURNING cnt"
+            ).then(count => {
+                res.send("There are " + count.cnt + " visits")
+            })
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
